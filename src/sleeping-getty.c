b3a6e63e81be2dc061559f1f9f4c44fe3f549652
Mattias Andrée <maandree@member.fsf.org>
2015-12-19 02:07:42 +0100

    add code
    Signed-off-by: Mattias Andrée <maandree@member.fsf.org>

---
+++ src/sleeping-getty.c
@@ -0,0 +1,42 @@
+/**
+ * Copyright © 2015  Mattias Andrée (maandree@member.fsf.org)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stropts.h>
+#include <linux/vt.h>
+
+
+
+int main(int argc, char* argv[])
+{
+  struct vt_stat state;
+  int tty;
+  
+  if (argc < 3)
+    return 2;
+  
+  tty = atoi(argv[1]);
+  if (!ioctl(STDIN_FILENO, (long)VT_GETSTATE, &state))
+    if (state.v_active != tty)
+      ioctl(STDIN_FILENO, (long)VT_WAITACTIVE, tty);
+  
+  execvp(argv[2], argv + 2);
+  perror(argv[0]);
+  return 1;
+}
+
